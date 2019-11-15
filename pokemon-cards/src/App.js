f2db45d41cd9f1fb7bbe1d31f420f7ea13644931
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-15 13:01:32 -0500

    pokemon API call success

--- pokemon-cards/src/App.js
+++ pokemon-cards/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/10/")
+      .then(res => {
+        const pokemon = res.data
+        console.log("Pokemon Data: ", pokemon)
+        setPokemon(pokemon);
+      })
+      .catch(err => {
+        console.log("An error has occured", err);
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Gotta Catch em' All</h1>
