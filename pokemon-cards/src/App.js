f168e589bc2e9a33bf4dab16d98a8b20dee082b5
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-15 13:44:22 -0500

    trying to get all info to display, currently have names

--- pokemon-cards/src/App.js
+++ pokemon-cards/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import axios from "axios";
 
 function App() {
@@ -7,21 +7,34 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon/10/")
+      .get("https://pokeapi.co/api/v2/pokemon/")
       .then(res => {
-        const pokemon = res.data
-        console.log("Pokemon Data: ", pokemon)
+        const pokemon = res.data.results;
+        console.log("Pokemon Data: ", pokemon);
         setPokemon(pokemon);
       })
       .catch(err => {
         console.log("An error has occured", err);
-      })
-  }, [])
-
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>Gotta Catch em' All</h1>
+      <div className="card-container">
+        {pokemon.map((pokemon, index) => {
+          return (
+            <div>
+              <h2>{pokemon.name}</h2>
+              <img
+                className="poke-pic"
+                src={pokemon.url}
+                alt="picture of pokemon"
+              ></img>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- pokemon-cards/src/index.js
+++ pokemon-cards/src/index.js
@@ -1,27 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 
-import reducer from "../src/reducers/reducer";
-
-const store = createStore(reducer, applyMiddleware(thunk));
-console.log(store);
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
